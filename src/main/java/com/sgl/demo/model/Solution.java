980302919f9d0c1d6805d922d7d264e07e8738df
Robert <haorongyi@126.com>
2021-07-24 11:05:12 +0800

    implement the part D counting animals

--- src/main/java/com/sgl/demo/model/Solution.java
+++ src/main/java/com/sgl/demo/model/Solution.java
@@ -1,5 +1,10 @@
 package com.sgl.demo.model;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -8,5 +13,27 @@ public class Solution {
         bird.walk();
         bird.fly();
         bird.sing();
+
+        List<Animal> animalList = Arrays.asList(new Fish(), new Bird(), new Duck(),
+                new Dog(), new Cat(), new Chicken(), new Dolphin(), new Shark(),
+                new ClownFish(), new ButterFly(), new Caterpillar());
+
+        AtomicInteger walkAnimalCount = new AtomicInteger();
+        AtomicInteger flyAnimalCount = new AtomicInteger();
+        AtomicInteger swimAnimalCount = new AtomicInteger();
+        AtomicInteger singAnimalCount = new AtomicInteger();
+
+        Consumer<Animal> animalConsumer = animal -> {
+            if (animal instanceof WalkAnimal) walkAnimalCount.getAndIncrement();
+            if (animal instanceof FlyAnimal) flyAnimalCount.getAndIncrement();
+            if (animal instanceof SwimAnimal) swimAnimalCount.getAndIncrement();
+            if (animal instanceof SingingAnimal) singAnimalCount.getAndIncrement();
+        };
+
+        animalList.forEach(animalConsumer);
+        System.out.println("no of walk animals:" + walkAnimalCount);
+        System.out.println("no of fly animals:" + flyAnimalCount);
+        System.out.println("no of swim animals:" + swimAnimalCount);
+        System.out.println("no of sing animals:" + singAnimalCount);
     }
 }
